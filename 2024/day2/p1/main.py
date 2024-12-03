a1e64a986cf6694cc6f9770ff8f484b47ca414b5
jadoox <martin.sohl@hotmail.com>
2024-12-03 17:29:15 +0100

    day 2 p1 done

--- 2024/day2/p1/main.py
+++ 2024/day2/p1/main.py
@@ -1,20 +1,38 @@
 row_list = []
 
-file = open("input_small.txt", "r")
+file = open("input.txt", "r")
 
 input_list = file.readlines()
+
+safe_count = 0
 
 for row in input_list:
     split_list = row.split()
 
     l1 = []
 
-    # generates list with int's
     for i in split_list:
         l1.append(int(i))
 
-    for i, j in enumerate(l1[:-1]):
-        if j  == l1[i+1]: 
-            print("smaller")
+    is_increasing = True
+    is_decreasing = True
 
-    print("----")
+    for i in range(len(l1) - 1):
+        current = l1[i]
+        next_item = l1[i + 1]
+        
+        diff = next_item - current
+        
+        if abs(diff) < 1 or abs(diff) > 3:
+            is_increasing = is_decreasing = False
+            break
+        
+        if diff > 0:
+            is_decreasing = False
+        elif diff < 0:
+            is_increasing = False
+
+    if (is_increasing or is_decreasing) and (len(l1) > 1):
+        safe_count = safe_count + 1
+
+print(safe_count)
