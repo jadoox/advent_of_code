ff4354a01a62a3da0a85e94b775faae4d1803a04
jadoox <martin.sohl@hotmail.com>
2024-12-08 19:48:35 +0100

    final sol

--- 2024/day2/p2/main.py
+++ 2024/day2/p2/main.py
@@ -14,38 +14,45 @@ for row in input_list:
     for i in split_list:
         l1.append(int(i))
 
-    fail_count = 0
-    is_increasing = 0
-    is_decreasing = 0
+    alternate_lists = []
 
-    for i in range(len(l1) - 1):
-        current = l1[i]
-        next_item = l1[i + 1]
-        
-        diff = next_item - current
+    for i in range(len(l1)):
+        new_list = l1[:i] + l1[i+1:]
+        alternate_lists.append(new_list)
 
-        if fail_count > 1:
-            break
+    alt_safe = 0
 
-        if diff > 0:
-            if is_decreasing > 0:
-                fail_count = fail_count + 1
-            is_increasing = is_increasing + 1
+    for list in alternate_lists:
+        fail = False
+        is_increasing = 0
+        is_decreasing = 0
 
-        if diff < 0:
-            if is_increasing > 0:
-                fail_count = fail_count + 1
-            is_decreasing = is_decreasing + 1
-
-        if abs(diff) < 1 or abs(diff) > 3 or abs(diff) == 0:
-            fail_count = fail_count + 1
-            continue
+        for num in range(len(list) - 1):
+            current = list[num]
+            next_item = list[num + 1]
             
-    if fail_count < 2:
-        print(l1)
+            diff = next_item - current
+
+            if diff > 0:
+                if is_decreasing > 0:
+                    fail = True
+                    break
+                is_increasing = is_increasing + 1
+
+            if diff < 0:
+                if is_increasing > 0:
+                    fail = True
+                    break
+                is_decreasing = is_decreasing + 1
+
+            if abs(diff) < 1 or abs(diff) > 3 or abs(diff) == 0:
+                fail = True
+                break
+                
+        if fail == False:
+            alt_safe = alt_safe + 1
+
+    if alt_safe > 0:
         safe_count = safe_count + 1
-
-    print("----")
-
 
 print(safe_count)
